79f5835f7c629a5e0ea71b2d6d801bf7d0988975
guzx <hnguzx@qq.com>
2021-06-17 19:21:09 +0800

    搭建环境

---
+++ getaway/src/main/java/pers/guzx/getaway/GetawayApplication.java
@@ -0,0 +1,16 @@
+package pers.guzx.getaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class GetawayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetawayApplication.class, args);
+    }
+
+}
